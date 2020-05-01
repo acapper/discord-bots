fd149557ee090b300b100debb79c0511f828807e
Antony Capper <acapper56@gmail.com>
2020-05-01 21:47:25 +0100

    Added template config file

---
+++ config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  discord: {
+    botkeys: [
+      {
+        key: 'API_KEY',
+        type: 'cat',
+        prefix: '!',
+      },
+      {
+        key: 'API_KEY',
+        type: 'dog',
+        prefix: '?',
+      },
+    ],
+  },
+  catapi: {
+    apikey: 'API_KEY',
+  },
+  dogapi: {
+    apikey: 'API_KEY',
+  },
+};
